da2c6628b9c644c5fc2c5342a8e510ea01ce1a28
dhanar98 <dhanasekarravi98@gmail.com>
2024-01-28 00:38:04 +0530

    routes added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RazorpayController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('payment',[RazorpayController::class,'index']);
+Route::post('payment/create',[RazorpayController::class,'store'])->name('razorpay.payment.store');
+Route::post('payment/failure',[RazorpayController::class,'failure'])->name('razorpay.payment.failure');
+Route::get('/402', function () {
+    return view('errors.402');
 });
